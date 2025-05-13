6c7c0e4ca1dd475abab547b02a6ec7f614be0b23
Arthur ALEXANDRE <arthur.alexandre682006@gmail.com>
2025-05-13 10:19:57 +0200

    Volet3 opérationnel + début charte graphique + ajout images véhicules pompier

--- Caserne/Caserne/Volet_3/FormParcoursEngins.Designer.cs
+++ Caserne/Caserne/Volet_3/FormParcoursEngins.Designer.cs
@@ -32,169 +32,183 @@
             this.lblChoixCaserne = new System.Windows.Forms.Label();
             this.cmbChoixCaserne = new System.Windows.Forms.ComboBox();
             this.grpInfosEngin = new System.Windows.Forms.GroupBox();
+            this.cbEnPanne = new System.Windows.Forms.CheckBox();
+            this.cbEnMission = new System.Windows.Forms.CheckBox();
             this.lblDateRCP = new System.Windows.Forms.Label();
+            this.lblNumeroEngin = new System.Windows.Forms.Label();
             this.lblEnMission = new System.Windows.Forms.Label();
             this.lblEnPanne = new System.Windows.Forms.Label();
-            this.lblNumEngin = new System.Windows.Forms.Label();
             this.bttnPrecedent = new System.Windows.Forms.Button();
             this.bttnSuivant = new System.Windows.Forms.Button();
-            this.lblNumero = new System.Windows.Forms.Label();
-            this.lblDate = new System.Windows.Forms.Label();
-            this.cbEnMission = new System.Windows.Forms.CheckBox();
-            this.cbEnPanne = new System.Windows.Forms.CheckBox();
+            this.pbEngin = new System.Windows.Forms.PictureBox();
             this.grpChoixCaserne.SuspendLayout();
             this.grpInfosEngin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEngin)).BeginInit();
             this.SuspendLayout();
             // 
             // grpChoixCaserne
             // 
             this.grpChoixCaserne.Controls.Add(this.lblChoixCaserne);
             this.grpChoixCaserne.Controls.Add(this.cmbChoixCaserne);
-            this.grpChoixCaserne.Location = new System.Drawing.Point(26, 38);
+            this.grpChoixCaserne.Location = new System.Drawing.Point(32, 54);
+            this.grpChoixCaserne.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.grpChoixCaserne.Name = "grpChoixCaserne";
-            this.grpChoixCaserne.Size = new System.Drawing.Size(762, 46);
+            this.grpChoixCaserne.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.grpChoixCaserne.Size = new System.Drawing.Size(952, 67);
             this.grpChoixCaserne.TabIndex = 0;
             this.grpChoixCaserne.TabStop = false;
             // 
             // lblChoixCaserne
             // 
             this.lblChoixCaserne.AutoSize = true;
-            this.lblChoixCaserne.Location = new System.Drawing.Point(24, 18);
+            this.lblChoixCaserne.Location = new System.Drawing.Point(30, 25);
+            this.lblChoixCaserne.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblChoixCaserne.Name = "lblChoixCaserne";
-            this.lblChoixCaserne.Size = new System.Drawing.Size(131, 16);
+            this.lblChoixCaserne.Size = new System.Drawing.Size(177, 23);
             this.lblChoixCaserne.TabIndex = 4;
             this.lblChoixCaserne.Text = "Choix de la caserne :";
             // 
             // cmbChoixCaserne
             // 
+            this.cmbChoixCaserne.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbChoixCaserne.FormattingEnabled = true;
-            this.cmbChoixCaserne.Location = new System.Drawing.Point(240, 18);
+            this.cmbChoixCaserne.Location = new System.Drawing.Point(305, 22);
+            this.cmbChoixCaserne.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cmbChoixCaserne.Name = "cmbChoixCaserne";
-            this.cmbChoixCaserne.Size = new System.Drawing.Size(512, 24);
+            this.cmbChoixCaserne.Size = new System.Drawing.Size(639, 31);
             this.cmbChoixCaserne.TabIndex = 0;
             this.cmbChoixCaserne.SelectedIndexChanged += new System.EventHandler(this.cmbChoixCaserne_SelectedIndexChanged_1);
             // 
             // grpInfosEngin
             // 
+            this.grpInfosEngin.Controls.Add(this.pbEngin);
             this.grpInfosEngin.Controls.Add(this.cbEnPanne);
             this.grpInfosEngin.Controls.Add(this.cbEnMission);
-            this.grpInfosEngin.Controls.Add(this.lblDate);
-            this.grpInfosEngin.Controls.Add(this.lblNumero);
             this.grpInfosEngin.Controls.Add(this.lblDateRCP);
+            this.grpInfosEngin.Controls.Add(this.lblNumeroEngin);
             this.grpInfosEngin.Controls.Add(this.lblEnMission);
             this.grpInfosEngin.Controls.Add(this.lblEnPanne);
-            this.grpInfosEngin.Controls.Add(this.lblNumEngin);
-            this.grpInfosEngin.Location = new System.Drawing.Point(26, 117);
+            this.grpInfosEngin.Location = new System.Drawing.Point(32, 168);
+            this.grpInfosEngin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.grpInfosEngin.Name = "grpInfosEngin";
-            this.grpInfosEngin.Size = new System.Drawing.Size(762, 240);
+            this.grpInfosEngin.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.grpInfosEngin.Size = new System.Drawing.Size(952, 345);
             this.grpInfosEngin.TabIndex = 1;
             this.grpInfosEngin.TabStop = false;
+            // 
+            // cbEnPanne
+            // 
+            this.cbEnPanne.AutoSize = true;
+            this.cbEnPanne.Enabled = false;
+            this.cbEnPanne.Location = new System.Drawing.Point(251, 271);
+            this.cbEnPanne.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbEnPanne.Name = "cbEnPanne";
+            this.cbEnPanne.Size = new System.Drawing.Size(18, 17);
+            this.cbEnPanne.TabIndex = 7;
+            this.cbEnPanne.UseVisualStyleBackColor = true;
+            // 
+            // cbEnMission
+            // 
+            this.cbEnMission.AutoSize = true;
+            this.cbEnMission.Enabled = false;
+            this.cbEnMission.Location = new System.Drawing.Point(251, 213);
+            this.cbEnMission.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbEnMission.Name = "cbEnMission";
+            this.cbEnMission.Size = new System.Drawing.Size(18, 17);
+            this.cbEnMission.TabIndex = 6;
+            this.cbEnMission.UseVisualStyleBackColor = true;
             // 
             // lblDateRCP
             // 
             this.lblDateRCP.AutoSize = true;
-            this.lblDateRCP.Location = new System.Drawing.Point(24, 70);
+            this.lblDateRCP.Location = new System.Drawing.Point(30, 113);
+            this.lblDateRCP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDateRCP.Name = "lblDateRCP";
-            this.lblDateRCP.Size = new System.Drawing.Size(114, 16);
-            this.lblDateRCP.TabIndex = 3;
-            this.lblDateRCP.Text = "Date de récéption";
+            this.lblDateRCP.Size = new System.Drawing.Size(60, 23);
+            this.lblDateRCP.TabIndex = 5;
+            this.lblDateRCP.Text = "label2";
+            // 
+            // lblNumeroEngin
+            // 
+            this.lblNumeroEngin.AutoSize = true;
+            this.lblNumeroEngin.Location = new System.Drawing.Point(30, 58);
+            this.lblNumeroEngin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNumeroEngin.Name = "lblNumeroEngin";
+            this.lblNumeroEngin.Size = new System.Drawing.Size(60, 23);
+            this.lblNumeroEngin.TabIndex = 4;
+            this.lblNumeroEngin.Text = "label1";
             // 
             // lblEnMission
             // 
             this.lblEnMission.AutoSize = true;
-            this.lblEnMission.Location = new System.Drawing.Point(24, 148);
+            this.lblEnMission.Location = new System.Drawing.Point(30, 213);
+            this.lblEnMission.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblEnMission.Name = "lblEnMission";
-            this.lblEnMission.Size = new System.Drawing.Size(72, 16);
+            this.lblEnMission.Size = new System.Drawing.Size(97, 23);
             this.lblEnMission.TabIndex = 2;
             this.lblEnMission.Text = "En mission";
             // 
             // lblEnPanne
             // 
             this.lblEnPanne.AutoSize = true;
-            this.lblEnPanne.Location = new System.Drawing.Point(24, 189);
+            this.lblEnPanne.Location = new System.Drawing.Point(30, 271);
+            this.lblEnPanne.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblEnPanne.Name = "lblEnPanne";
-            this.lblEnPanne.Size = new System.Drawing.Size(64, 16);
+            this.lblEnPanne.Size = new System.Drawing.Size(84, 23);
             this.lblEnPanne.TabIndex = 1;
             this.lblEnPanne.Text = "En panne";
             // 
-            // lblNumEngin
-            // 
-            this.lblNumEngin.AutoSize = true;
-            this.lblNumEngin.Location = new System.Drawing.Point(24, 28);
-            this.lblNumEngin.Name = "lblNumEngin";
-            this.lblNumEngin.Size = new System.Drawing.Size(55, 16);
-            this.lblNumEngin.TabIndex = 0;
-            this.lblNumEngin.Text = "Numéro";
-            // 
             // bttnPrecedent
             // 
-            this.bttnPrecedent.Location = new System.Drawing.Point(306, 388);
+            this.bttnPrecedent.Location = new System.Drawing.Point(382, 558);
+            this.bttnPrecedent.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.bttnPrecedent.Name = "bttnPrecedent";
-            this.bttnPrecedent.Size = new System.Drawing.Size(75, 21);
+            this.bttnPrecedent.Size = new System.Drawing.Size(94, 30);
             this.bttnPrecedent.TabIndex = 2;
             this.bttnPrecedent.Text = "<--";
             this.bttnPrecedent.UseVisualStyleBackColor = true;
+            this.bttnPrecedent.Click += new System.EventHandler(this.bttnPrecedent_Click);
             // 
             // bttnSuivant
             // 
-            this.bttnSuivant.Location = new System.Drawing.Point(427, 388);
+            this.bttnSuivant.Location = new System.Drawing.Point(534, 558);
+            this.bttnSuivant.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.bttnSuivant.Name = "bttnSuivant";
-            this.bttnSuivant.Size = new System.Drawing.Size(75, 21);
+            this.bttnSuivant.Size = new System.Drawing.Size(94, 30);
             this.bttnSuivant.TabIndex = 3;
             this.bttnSuivant.Text = "-->";
             this.bttnSuivant.UseVisualStyleBackColor = true;
+            this.bttnSuivant.Click += new System.EventHandler(this.bttnSuivant_Click);
             // 
-            // lblNumero
+            // pbEngin
             // 
-            this.lblNumero.AutoSize = true;
-            this.lblNumero.Location = new System.Drawing.Point(198, 28);
-            this.lblNumero.Name = "lblNumero";
-            this.lblNumero.Size = new System.Drawing.Size(44, 16);
-            this.lblNumero.TabIndex = 4;
-            this.lblNumero.Text = "label1";
-            // 
-            // lblDate
-            // 
-            this.lblDate.AutoSize = true;
-            this.lblDate.Location = new System.Drawing.Point(198, 70);
-            this.lblDate.Name = "lblDate";
-            this.lblDate.Size = new System.Drawing.Size(44, 16);
-            this.lblDate.TabIndex = 5;
-            this.lblDate.Text = "label2";
-            // 
-            // cbEnMission
-            // 
-            this.cbEnMission.AutoSize = true;
-            this.cbEnMission.Location = new System.Drawing.Point(201, 148);
-            this.cbEnMission.Name = "cbEnMission";
-            this.cbEnMission.Size = new System.Drawing.Size(18, 17);
-            this.cbEnMission.TabIndex = 6;
-            this.cbEnMission.UseVisualStyleBackColor = true;
-            // 
-            // cbEnPanne
-            // 
-            this.cbEnPanne.AutoSize = true;
-            this.cbEnPanne.Location = new System.Drawing.Point(201, 189);
-            this.cbEnPanne.Name = "cbEnPanne";
-            this.cbEnPanne.Size = new System.Drawing.Size(18, 17);
-            this.cbEnPanne.TabIndex = 7;
-            this.cbEnPanne.UseVisualStyleBackColor = true;
+            this.pbEngin.Location = new System.Drawing.Point(429, 52);
+            this.pbEngin.Name = "pbEngin";
+            this.pbEngin.Size = new System.Drawing.Size(492, 258);
+            this.pbEngin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbEngin.TabIndex = 8;
+            this.pbEngin.TabStop = false;
             // 
             // FormParcoursEngins
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1000, 646);
             this.Controls.Add(this.bttnSuivant);
             this.Controls.Add(this.bttnPrecedent);
             this.Controls.Add(this.grpInfosEngin);
             this.Controls.Add(this.grpChoixCaserne);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FormParcoursEngins";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormParcoursEngins";
             this.grpChoixCaserne.ResumeLayout(false);
             this.grpChoixCaserne.PerformLayout();
             this.grpInfosEngin.ResumeLayout(false);
             this.grpInfosEngin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEngin)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -205,15 +219,14 @@
         private System.Windows.Forms.GroupBox grpInfosEngin;
         private System.Windows.Forms.Label lblChoixCaserne;
         private System.Windows.Forms.ComboBox cmbChoixCaserne;
-        private System.Windows.Forms.Label lblDateRCP;
         private System.Windows.Forms.Label lblEnMission;
         private System.Windows.Forms.Label lblEnPanne;
-        private System.Windows.Forms.Label lblNumEngin;
         private System.Windows.Forms.Button bttnPrecedent;
         private System.Windows.Forms.Button bttnSuivant;
-        private System.Windows.Forms.Label lblDate;
-        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label lblDateRCP;
+        private System.Windows.Forms.Label lblNumeroEngin;
         private System.Windows.Forms.CheckBox cbEnPanne;
         private System.Windows.Forms.CheckBox cbEnMission;
+        private System.Windows.Forms.PictureBox pbEngin;
     }
 }
--- Caserne/Caserne/Volet_3/FormParcoursEngins.cs
+++ Caserne/Caserne/Volet_3/FormParcoursEngins.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Data.SQLite;
+using System.IO;
 
 namespace Caserne
 {
@@ -18,6 +19,19 @@ namespace Caserne
         {
             InitializeComponent();
             ChargerCasernes();
+        }
+
+        private List<Engin> engins = new List<Engin>();
+        private int indexActuel = 0;
+
+        private class Engin
+        {
+            public string CodeTypeEngin { get; set; }
+            public string Numero { get; set; }
+            public DateTime DateReception { get; set; }
+            public int IdCaserne { get; set; }
+            public bool EnMission { get; set; }
+            public bool EnPanne { get; set; }
         }
 
         private void ChargerCasernes()
@@ -56,47 +70,107 @@ namespace Caserne
         {
             try
             {
-                // Récupérer la connexion
+                engins.Clear();
+                indexActuel = 0;
+
                 SQLiteConnection connection = Connexion.Connec;
 
-                // Préparer la requête SQL avec une jointure entre Engin et Caserne
                 string requete = @"
-                    SELECT e.codeTypeEngin, e.numero, e.dateReception, e.idCaserne
+                    SELECT e.codeTypeEngin, e.numero, e.dateReception, e.idCaserne, e.enMission, e.enPanne
                     FROM Engin e
                     INNER JOIN Caserne c ON e.idCaserne = c.id
                     WHERE c.nom = @caserneNom;
                 ";
 
                 SQLiteCommand command = new SQLiteCommand(requete, connection);
-                command.Parameters.AddWithValue("@caserneNom", caserneNom);  // Remplacer avec le nom de la caserne sélectionnée
+                command.Parameters.AddWithValue("@caserneNom", caserneNom);
 
                 SQLiteDataReader reader = command.ExecuteReader();
 
-                // S'assurer que la GroupBox contient déjà des Label pour afficher les données
-                if (reader.HasRows)
+                while (reader.Read())
                 {
-                    // Récupérer les premières données et les afficher dans les Labels
-                    reader.Read();  // Lire la première ligne de résultat
+                    engins.Add(new Engin
+                    {
+                        CodeTypeEngin = reader["codeTypeEngin"].ToString(),
+                        Numero = reader["numero"].ToString(),
+                        DateReception = Convert.ToDateTime(reader["dateReception"]),
+                        IdCaserne = Convert.ToInt32(reader["idCaserne"]),
+                        EnMission = Convert.ToBoolean(reader["enMission"]),
+                        EnPanne = Convert.ToBoolean(reader["enPanne"])
+                    });
 
-                    // Mettre à jour les labels existants avec les données de la base
-                    lblNumero.Text = reader["idCaserne"].ToString() + 
-                        " - " +
-                        reader["codeTypeEngin"].ToString() +
-                        " - " +
-                        reader["numero"].ToString();
-                    lblDate.Text = "Date de Réception: " + Convert.ToDateTime(reader["dateReception"]).ToString("dd/MM/yyyy");
-                }
-                else
-                {
-                    // Si aucune donnée n'est trouvée
-                    MessageBox.Show("Aucun engin trouvé pour la caserne spécifiée.");
                 }
 
                 reader.Close();
+
+                if (engins.Count > 0)
+                {
+                    AfficherEngin(indexActuel);
+                }
+                else
+                {
+                    MessageBox.Show("Aucun engin trouvé pour la caserne spécifiée.");
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Erreur lors de la récupération des engins : " + ex.Message);
+            }
+        }
+        private void AfficherEngin(int index)
+        {
+            if (index >= 0 && index < engins.Count)
+            {
+                var engin = engins[index];
+
+                lblNumeroEngin.Text = $"Numéro  :  {engin.IdCaserne} - {engin.CodeTypeEngin} - {engin.Numero}";
+                lblDateRCP.Text = $"Date de Réception  :  {engin.DateReception:dd/MM/yyyy}";
+                cbEnMission.Checked = engin.EnMission;
+                cbEnPanne.Checked = engin.EnPanne;
+
+                // Chargement de l'image
+                string dossierImages = @"C:\Users\arthu\A21\Images\FormParcoursEngins\ImagesEngins";
+                string cheminImage = Path.Combine(dossierImages, engin.CodeTypeEngin + ".png");
+
+                if (pbEngin.Image != null)
+                {
+                    pbEngin.Image.Dispose();
+                    pbEngin.Image = null;
+                }
+
+                if (File.Exists(cheminImage))
+                {
+                    pbEngin.Image = Image.FromFile(cheminImage);
+                }
+                else
+                {
+                    // Image par défaut
+                    string defaultPath = Path.Combine(dossierImages, "default.png");
+                    if (File.Exists(defaultPath))
+                    {
+                        pbEngin.Image = Image.FromFile(defaultPath);
+                    }
+                }
+            }
+        }
+
+
+
+        private void bttnPrecedent_Click(object sender, EventArgs e)
+        {
+            if (indexActuel > 0)
+            {
+                indexActuel--;
+                AfficherEngin(indexActuel);
+            }
+        }
+
+        private void bttnSuivant_Click(object sender, EventArgs e)
+        {
+            if (indexActuel < engins.Count - 1)
+            {
+                indexActuel++;
+                AfficherEngin(indexActuel);
             }
         }
     }
